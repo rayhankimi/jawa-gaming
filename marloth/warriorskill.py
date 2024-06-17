0a09f471add5b4a293985f6a4c1db9911f59c2ec
Rayhan K <166932170+rayhankimi@users.noreply.github.com>
2024-06-17 18:03:28 +0800

    fixed damage calc for warrior

--- marloth/warriorskill.py
+++ marloth/warriorskill.py
@@ -2,16 +2,26 @@ from marloth import playerattr as pa, playerstat as ps
 
 
 def provoke(base=5) -> int:
-    vitMul = ps.vitality() * 0.1
-    strnMul = ps.strength() * 0.05
-    damage = base + (base * vitMul) + (base * strnMul) + (pa.patk() * 0.3)
+    patk = pa.patk() * 0.05
+    strnmul = ps.strength() * 0.05
+    vitmul = ps.vitality() * 0.1
+    agimul = ps.agility() * 0
+    dexmul = ps.dexterity() * 0.05
+    base = base + patk
+    damage = base + (base * strnmul) + (base * vitmul) + (base + agimul) + (base + dexmul)
+
     return round(damage)
 
 
 def rendersword(base=12) -> int:
-    strnMul = ps.strength() * 0.1
-    atkmul = pa.patk() * 0.15
-    damage = base + (atkmul * strnMul)
+    patk = pa.patk() * 0.1
+    strnmul = ps.strength() * 0.08
+    vitmul = ps.vitality() * 0.03
+    agimul = ps.agility() * 0.03
+    dexmul = ps.dexterity() * 0.03
+    base = base + patk
+    damage = base + (base * strnmul) + (base * vitmul) + (base + agimul) + (base + dexmul)
+
     return round(damage)
 
 
@@ -21,20 +31,31 @@ def combosword(base=15) -> int:
     vitmul = ps.vitality() * 0.05
     agimul = ps.agility() * 0.05
     dexmul = ps.dexterity() * 0.05
-
     base = base + patk
     damage = base + (base * strnmul) + (base * vitmul) + (base + agimul) + (base + dexmul)
+
     return round(damage)
 
 
 def cutthrough(base=10) -> int:
-    strnMul = ps.strength() * 0.15
-    damage = base + (base * strnMul) + (pa.patk() * 0.3)
+    patk = pa.patk() * 0.15
+    strnmul = ps.strength() * 0.075
+    vitmul = ps.vitality() * 0.025
+    agimul = ps.agility() * 0.025
+    dexmul = ps.dexterity() * 0.025
+    base = base + patk
+    damage = base + (base * strnmul) + (base * vitmul) + (base + agimul) + (base + dexmul)
+
     return round(damage)
 
 
 def burningoath(base=15) -> int:
-    vitMul = ps.vitality() * 0.1
-    strnMul = ps.strength() * 0.05
-    damage = base + (base * vitMul) + (base * strnMul) + (pa.hp() * 0.01)
+    patk = pa.patk() * 0.15
+    strnmul = ps.strength() * 0.075
+    vitmul = ps.vitality() * 0.025
+    agimul = ps.agility() * 0.025
+    dexmul = ps.dexterity() * 0.025
+    base = base + patk
+    damage = base + (base * strnmul) + (base * vitmul) + (base + agimul) + (base + dexmul)
+
     return round(damage)

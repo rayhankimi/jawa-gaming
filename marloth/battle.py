3fc150be2c6f83e4c3c620d9ae6218cade82fd43
Rayhan K <166932170+rayhankimi@users.noreply.github.com>
2024-06-18 18:00:49 +0800

    updated mechanics fro player reduction

--- marloth/battle.py
+++ marloth/battle.py
@@ -5,11 +5,20 @@ from marloth import enemyattr as en, enemyskill as es, playerattr as pa, warrior
 from marloth import status
 
 
-def mechanics(hp, damage=0) -> int:
+def attackMechanics(hp, damage=0) -> int:
     reduction = en.enemyreductions()
-    finaldamage = damage * ((100 - reduction) / 100)
-    finalhp = hp - finaldamage
-    return round(finalhp)
+    finaldamage = round(damage * ((100 - reduction) / 100))
+    finalhp = round(hp - finaldamage)
+    print(f"{finaldamage} , Enemy Hp : {finalhp}")
+    return finalhp
+
+
+def defenseMechanics(hp, damage=0) -> int:
+    reduction = pa.playerReductions()
+    finaldamage = round(damage * ((100 - reduction) / 100))
+    finalhp = round(hp - finaldamage)
+    print(f"{finaldamage} | Your Hp {hp}")
+    return finalhp
 
 
 def skillSelect(enemyhp, skill=None) -> int:
@@ -22,28 +31,30 @@ def skillSelect(enemyhp, skill=None) -> int:
 
         if skill == 1:
             damage = warriorskill.provoke()
-            enemyhp = mechanics(enemyhp, damage)
-            print(f"Provoke Damage : {damage} | Enemy Hp {enemyhp}")
+            print("You cast Provoke and dealt :", end=" ")
+            enemyhp = attackMechanics(enemyhp, damage)
 
         elif skill == 2:
             damage = warriorskill.rendersword()
-            enemyhp = mechanics(enemyhp, damage)
-            print(f"Render Sword Damage : {damage} | Enemy Hp {enemyhp}")
+            print("You cast Render Sword and dealt :", end=" ")
+            enemyhp = attackMechanics(enemyhp, damage)
 
         elif skill == 3:
             damage = warriorskill.combosword()
-            enemyhp = mechanics(enemyhp, damage)
-            print(f"Combo Sword Damage : {damage} | Enemy Hp {enemyhp}")
+            print("You cast Combo Sword and dealt :", end=" ")
+            enemyhp = attackMechanics(enemyhp, damage)
 
         elif skill == 4:
             damage = warriorskill.cutthrough()
-            enemyhp = mechanics(enemyhp, damage)
-            print(f"Cut Through Damage : {damage} | Enemy Hp {enemyhp}")
+            print("You cast Cut Through and dealt :", end=" ")
+            enemyhp = attackMechanics(enemyhp, damage)
+
 
         elif skill == 5:
             damage = warriorskill.burningoath()
-            enemyhp = mechanics(enemyhp, damage)
-            print(f"Burning Oath Damage : {damage} | Enemy Hp {enemyhp}")
+            print("You cast Burning Oath and dealt :", end=" ")
+            enemyhp = attackMechanics(enemyhp, damage)
+
 
         elif skill == 0:
             print("You did nothing!")
@@ -55,42 +66,42 @@ def skillSelect(enemyhp, skill=None) -> int:
     return enemyhp
 
 
-def enemySkill(playerHp) -> int:
+def enemySkill(hp) -> int:
     enemyDo = rd.randint(1, 6)
     if enemyDo == 1:
         damageTaken = es.claw()
-        playerHp = mechanics(playerHp, damageTaken)
-        print(f"Enemy use claw and dealt {damageTaken} | Your Hp {playerHp}")
+        print(f"Enemy use claw and dealt :", end=" ")
+        hp = defenseMechanics(hp, damageTaken)
 
     elif enemyDo == 2:
         damageTaken = es.roar()
-        playerHp = mechanics(playerHp, damageTaken)
-        print(f"Enemy use roar and dealt {damageTaken} | Your Hp {playerHp}")
+        print(f"Enemy use roar and dealt :", end=" ")
+        hp = defenseMechanics(hp, damageTaken)
 
     elif enemyDo == 3:
         damageTaken = es.throw()
-        playerHp = mechanics(playerHp, damageTaken)
-        print(f"Enemy use throw and dealt {damageTaken} | Your Hp {playerHp}")
+        print(f"Enemy use throw and dealt :", end=" ")
+        hp = defenseMechanics(hp, damageTaken)
 
     elif enemyDo == 4:
         damageTaken = es.plunge()
-        playerHp = mechanics(playerHp, damageTaken)
-        print(f"Enemy use plunge and dealt {damageTaken} | Your Hp {playerHp}")
+        print(f"Enemy use plunge and dealt :", end=" ")
+        hp = defenseMechanics(hp, damageTaken)
 
     elif enemyDo == 5:
         damageTaken = es.slash()
-        playerHp = mechanics(playerHp, damageTaken)
-        print(f"Enemy use slash and dealt {damageTaken} | Your Hp {playerHp}")
+        print(f"Enemy use slash and dealt :", end=" ")
+        hp = defenseMechanics(hp, damageTaken)
 
     elif enemyDo == 6:
         damageTaken = es.chomp()
-        playerHp = mechanics(playerHp, damageTaken)
-        print(f"Enemy use chomp and dealt : {damageTaken} | Your Hp {playerHp}")
+        print(f"Enemy use chomp and dealt :", end=" ")
+        hp = defenseMechanics(hp, damageTaken)
 
     else:
         print("You evaded !")
 
-    return playerHp
+    return hp
 
 
 def turn(n) -> None:
